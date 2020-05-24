eb8c6c927cc37c9228018c8f67cd034c5e5587c6
Gustav Fredriksson <gustav.fredriksson@tink.se>
2020-05-24 20:35:44 +0200

    chore: add basic avanza paths

---
+++ avanza/avanzaConstants/avanzaConstants.go
@@ -0,0 +1,7 @@
+package avanzaConstants
+
+var (
+	URL_POSITIONS_PATH             = "/_mobile/account/positions"
+	URL_AUTHENTICATION_PATH        = "/_api/authentication/sessions/usercredentials"
+	URL_TOTP_PATH                  = "/_api/authentication/sessions/totp"
+)
